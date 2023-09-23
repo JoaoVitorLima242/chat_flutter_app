66a81e6b85a4ce1cb1dc5e4a335d8a338cc1f969
JoaoVitorLima242 <jlima0500@gmail.com>
2023-09-23 19:26:42 -0300

    feat: logout user

--- lib/features/chat/chat_screen.dart
+++ lib/features/chat/chat_screen.dart
@@ -1,12 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
+  void _onLogout() {
+    _firebase.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('FlutterChat')),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: _onLogout,
+            icon: const Icon(Icons.exit_to_app),
+          )
+        ],
+        title: const Text('FlutterChat'),
+      ),
       body: const Center(child: Text('Chat')),
     );
   }
