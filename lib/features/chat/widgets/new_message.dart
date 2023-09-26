ad06d61cd7438e95c9052beb5302d1a2673dbba8
JoaoVitorLima242 <jlima0500@gmail.com>
2023-09-26 09:07:39 -0300

    feat: sending message

--- lib/features/chat/widgets/new_message.dart
+++ lib/features/chat/widgets/new_message.dart
@@ -1,4 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+
+final _firebaseFirestore = FirebaseFirestore.instance;
+final _firebaseAuth = FirebaseAuth.instance;
+// final _firebaseStorage = FirebaseStorage.instance;
 
 class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
@@ -18,16 +25,47 @@ class _NewMessage extends State<NewMessage> {
     super.dispose();
   }
 
-  void _onSubmitMessage() {
+  void _onSubmitMessage() async {
     final enteredMessage = _messageController.text;
 
     if (enteredMessage.trim().isEmpty) {
       return;
     }
+    FocusScope.of(context).unfocus();
 
-    // send message () {}
-
+    final user = _firebaseAuth.currentUser!;
     _messageController.clear();
+
+    try {
+      final userData =
+          await _firebaseFirestore.collection('users').doc(user.uid).get();
+
+      if (userData.data()?['username'] == null &&
+          userData.data()?['image_url'] == null) {
+        print(userData);
+
+        return;
+      }
+
+      await _firebaseFirestore.collection('chat').add({
+        'text': enteredMessage,
+        'createdAt': Timestamp.now(),
+        'userId': user.uid,
+        'username': userData.data()!['username'],
+        'user_image': userData.data()!['image_url'],
+      });
+    } on FirebaseException catch (error) {
+      print(error);
+      openErrorSnackBar();
+      _messageController.clear();
+    }
+  }
+
+  void openErrorSnackBar() {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Error sending the message')),
+    );
   }
 
   @override
@@ -36,12 +74,13 @@ class _NewMessage extends State<NewMessage> {
       padding: const EdgeInsets.only(left: 20, right: 10, bottom: 40),
       child: Row(
         children: [
-          const Expanded(
+          Expanded(
             child: TextField(
               textCapitalization: TextCapitalization.sentences,
               autocorrect: true,
               enableSuggestions: true,
-              decoration: InputDecoration(labelText: 'Send a message...'),
+              controller: _messageController,
+              decoration: const InputDecoration(labelText: 'Send a message...'),
             ),
           ),
           IconButton(
