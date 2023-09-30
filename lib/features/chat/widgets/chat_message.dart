4ea494384a74f9adcb0b638f483016c7508ff9a9
JoaoVitorLima242 <jlima0500@gmail.com>
2023-09-29 22:35:19 -0300

    feat: displaying messages

--- lib/features/chat/widgets/chat_message.dart
+++ lib/features/chat/widgets/chat_message.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +7,35 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No message found.'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No message found.'),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong...'),
+          );
+        }
+
+        final loadedMessages = snapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) =>
+              Text(loadedMessages[index].data()['text']),
+        );
+      },
     );
   }
 }
